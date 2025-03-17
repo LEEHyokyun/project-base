238ba01b91847191ee44667983f8de1b565f3b8d
leehyokyun <gyrbs22@gmail.com>
2025-03-17 14:36:03 +0900

    base-tymeleaf project init

---
+++ src/main/java/com/hyokyunp1/hyokyunp1/Hyokyunp1Application.java
@@ -0,0 +1,13 @@
+package com.hyokyunp1.hyokyunp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hyokyunp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hyokyunp1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/hyokyunp1/hyokyunp1/Hyokyunp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyokyunp1.hyokyunp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hyokyunp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
